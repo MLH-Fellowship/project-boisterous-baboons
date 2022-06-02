9c2db5f3b589a7969ae7f7b761ec6878bcce4059
Juan Pablo Acosta <juanpabloacosta@Juans-MacBook-Air.local>
2022-06-02 01:12:17 -0600

    Added additional key-value pairs for education

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-team = {'juan': {"name": "Juan Acosta", "university": "University of Toronto", "github": "https://github.com/jpablo2002", "linkedin": "https://www.linkedin.com/in/juanp-acosta/",
+team = {'juan': {"name": "Juan Acosta", "university": "University of Toronto", "degree": "Computer Science", "years": 2, "github": "https://github.com/jpablo2002", "linkedin": "https://www.linkedin.com/in/juanp-acosta/",
                  "visited": ["Canada", "Venezuela", "Spain", "England", "Peru", "USA"], "skills": ["HTML", "CSS", "Javascript", "React", "Node.js", "MongoDB", "Python"]},
-        'malik': {"name": "Malik Baker", "university": "Boston University", "linkedin": "https://www.linkedin.com/in/malbaker/", "github": "https://www.github.com/malbaker/", "visited": ["Brazil", "Mexico", "Germany", "England", "Venezuela", "Dominican Republic", "Canada"], "skills": ["Python", "Swift", "C++", "HTML", "CSS", "Javascript"]},
-        'noah': {"name": "Noah Romo", "university": "Florida State University", "linkedin": "https://www.linkedin.com/in/noah-romo/", "github": "https://github.com/noahromo"}
+        'malik': {"name": "Malik Baker", "university": "Boston University", "degree": "Computer Science", "years": 3, "linkedin": "https://www.linkedin.com/in/malbaker/", "github": "https://www.github.com/malbaker/"},
+        'noah': {"name": "Noah Romo", "university": "Florida State University", "degree": "Computer Science", "years": 3, "linkedin": "https://www.linkedin.com/in/noah-romo/", "github": "https://github.com/noahromo", "visited": ["Brazil", "Mexico", "Germany", "England", "Venezuela", "Dominican Republic", "Canada"], "skills": ["Python", "Swift", "C++", "HTML", "CSS", "Javascript"]}
         }
 
 
