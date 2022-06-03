2924d86cd0416bbe292651ebbfe5e7cbba0ee49e
Juan Pablo Acosta <juanpabloacosta@Juans-MacBook-Air.local>
2022-06-03 15:26:05 -0600

    Moved map to individual pages and added new request to get member's visited locations and coloured icons

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ team = {'juan': {"name": "Juan Acosta",
                  "years": "2021-2025", "github": "https://github.com/jpablo2002",
                  "linkedin": "https://www.linkedin.com/in/juanp-acosta/",
                  "activities": " ",
-                 "visited": [("Canada", (56.13, -106.34)), ("Venezuela", (6.423, -66.58)), ("Spain", (40.46, -3.749)), ("England", (52.35, -1.17)), ("Peru", (-9.18, -75.0)), ("USA", (37.09, -95.71))],
+                 "visited": [["Canada", [56.13, -106.34]], ["Venezuela", [6.423, -66.58]], ["Spain", [40.46, -3.749]], ["England", [52.35, -1.17]], ["Peru", [-9.18, -75.0]], ["USA", [37.09, -95.71]]],
                  "skills": ["HTML", "CSS", "Javascript", "React", "Node.js", "MongoDB", "Python"],
                  "about": " ",
                  "seal": "toronto"},
@@ -22,7 +22,7 @@ team = {'juan': {"name": "Juan Acosta",
                   "linkedin": "https://www.linkedin.com/in/malbaker/",
                   "github": "https://www.github.com/malbaker/",
                   "activities": " ",
-                  "visited": [("USA", (37.09, -95.71)), ("Jamaica", 18.1, -77.29)],
+                  "visited": [["USA", [37.09, -95.71]], ["Jamaica", [18.1, -77.29]]],
                   "skills": ["Java", "Python", "C", "Linux", "Git", "Bash", "HTML", "CSS", "Bash"],
                   "about": " ",
                   "seal": "boston"},
@@ -33,7 +33,8 @@ team = {'juan': {"name": "Juan Acosta",
                  "linkedin": "https://www.linkedin.com/in/noah-romo/",
                  "github": "https://github.com/noahromo",
                  "activities": "Activities: Google Developer Student Club, Beta Theta Pi, FSU Boxing",
-                 "visited": [("Brazil", (-14.235, -51.925)), ("Mexico", (23.63, -102.5)), ("Germany", (51.16, 10.45)), ("England", (52.35, -1.17)), ("Venezuela", (6.423, -66.58)), ("Dominican Republic", (18.73, -70.16)), ("Canada", (56.13, -106.34))],
+                 "visited": [["Brazil", [-14.235, -51.925]], ["Mexico", [23.63, -102.5]], ["Germany", [51.16, 10.45]], ["England", [52.35, -1.17]],
+                             ["Venezuela", [6.423, -66.58]], ["Dominican Republic", [18.73, -70.16]], ["Canada", [56.13, -106.34]]],
                  "skills": ["Python", "Swift", "C++", "HTML", "CSS", "Javascript"],
                  "about": "Rising junior at Florida State University in pursuit of a Bachelor of Arts in Computer Science as well as a minor in innovation. App development is like trying to learn chess. So many different ideas/moves to pick from. I started as a beginner. After playing a few games each day, I've developed into a pretty decent player. Soon enough I'll be a master. I'm interested in tech entrepreneurship, fintech, mobile dev, machine learning applications, and decentralized applications. \"The people who are crazy enough to think that they can change the world are the ones who do.\" - Steve Jobs", "seal": "fsu"}
         }
@@ -60,3 +61,9 @@ def member(member):
                            skills=person["skills"],
                            about=person["about"],
                            seal=person["seal"])
+
+
+@app.route('/visited/<member>')
+def visited(member):
+    person = team[member]
+    return person
--- app/static/member.js
+++ app/static/member.js
@@ -4,6 +4,42 @@ let menuOpen = false;
 const burgerButton = document.querySelector('.burger-button');
 const menu = document.querySelector('.menu');
 
-burgerButton.addEventListener('click', (e) => {
+burgerButton.addEventListener('click', () => {
     menu.classList.toggle('open');
 })
+
+
+////////////////////////////////////////////////////////////
+// Map
+const map = L.map('map').setView([40, 0], 2.2);
+
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const colours = {
+    "juan": "violet",
+    "noah": "blue",
+    "malik": "green"
+}
+
+let visited;
+const member = window.location.href.split('/').slice(-1)[0];
+
+const memberIcon = new L.Icon({
+    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-${colours[member]}.png`,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+fetch(`/visited/${member}`)
+    .then((res) => res.json())
+    .then((data) => {
+        visited = data.visited;
+        visited.forEach((place) => {
+            L.marker(place[1], { icon: memberIcon }).addTo(map)
+        })
+    })
+    .catch(err => console.log(err))
