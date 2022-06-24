ed215f7996e7c21a2fdca3db4df320c0eb9b4818
Malik Baker <78194321+malbaker@users.noreply.github.com>
2022-06-24 16:36:32 -0400

    edited redeploy script to use restart systemd service instead of using tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-tmux kill-session -t portfolio
 cd flask-portfolio
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 clear
-tmux new -d -s  portfolio
-tmux send -t portfolio "flask run --host=0.0.0.0" ENTER
+systemctl restart myportfolio
 cd
 
 
