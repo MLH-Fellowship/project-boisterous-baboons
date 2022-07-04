52cbf95b4c31f13032dffd85b7f3c2125a8d55ee
Malik Baker <78194321+malbaker@users.noreply.github.com>
2022-07-04 14:17:26 -0400

    created Dockerfile

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd flask-portfolio
+
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 clear
 systemctl restart myportfolio
-cd
+
 
 
