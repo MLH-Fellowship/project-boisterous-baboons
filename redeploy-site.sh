521c186eb1a86e31e5bff13f9f44d784332d7398
Malik Baker <78194321+malbaker@users.noreply.github.com>
2022-07-04 16:57:49 -0400

    new redeployment script + small text change for test

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-
+cd flask-project
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-clear
-systemctl restart myportfolio
-
-
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
+cd
 
